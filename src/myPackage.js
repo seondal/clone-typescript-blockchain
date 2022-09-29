5ebd6fb80d486727372aa2887179183ba55554b4
seondal <sseondal@gmail.com>
2022-09-30 07:10:27 +0900

    타입스크립트 개발환경 구축

---
+++ src/myPackage.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+
+export function init(config) {
+  return true;
+}
+
+/**
+ * Exit the program
+ * @param {number} code
+ * @returns {number}
+ */
+
+export function exit(code) {
+  return code + 1;
+}
